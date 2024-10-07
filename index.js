d2a81ee8f9bbf525aacc2389bed078a185f18ced
MaheshChand123 <mchand0210@gmail.com>
2024-10-07 11:34:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+// console.log("hello world!");
+// let age = 23;
+
+// console.log(age);
+// console.log(country)
+
+// var country = "India";
+
+// console.log(country)
+// let user;
+// user = null;
+// console.log(user);
+
+let data;
+console.log(typeof data);
+console.log(data);
+
+//number
+let num1 = 50;
+let decimal_no = 1.0001;
+let pi = 3.14;
+let age = 25;
+let roll_no = 21;
+
+console.log(typeof num1);
+
+//substraction
+
+let num2 = 8;
+let num3 = 2;
+let substraction = num2-num3;
+console.log(substraction);
+
+// multiplication
+let multiplication = num2 * num3;
+console.log(multiplication);
+
+//division
+
+let division = num2/num3;
+console.log(division);
+
+//bigint
+
+let huge_no = 128778666846474647;
+console.log(typeof huge_no);
+
+//symbole
+
+let sym1 = symbol("sym1");
+let sym2 = symbol("sym1");
+console.log(sym1===sym2);
+
+//NaN
+
+let num5 = 0;
+let num6 = 0;
+console.log(num5 / num6);
+
